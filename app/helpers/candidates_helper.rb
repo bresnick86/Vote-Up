4e0bece68dff7ce2d6a838f88e3079289d3d17bc
Brandon Resnick <bresnick86@gmail.com>
2012-06-28 23:39:34 -0400

    third commit

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,2 +1,22 @@
 module CandidatesHelper
+	
+	def max_vote_count(candidates)
+		count = []
+
+		candidates.each do |c|
+			count << c.votes_count 
+		end
+		if count.max == 1 then vote = "vote" else vote = "votes" end
+		@winner_points = "Current Winner has #{count.max} #{vote}"
+	end
+
+	def total_votes(candidates)
+		count = 0
+
+		candidates.each do |c|
+			count += c.votes_count
+		end
+		count
+	end
+
 end
